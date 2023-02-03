c35070a5538bb4330dbf1ee29a1d972b77231e31
DavidCarG <davidzone23@gmail.com>
2023-02-03 16:10:03 -0600

    Make the cards check if they are eachothers match
    This was acomplished by having a counter that checked the number of cards flipped,
    if it was 2 then it forbid the user to flip any more cards and started checking
    whether they were eachothers match or not, if they were we left them flipped but
    disabled the option to flip them again, like in real life memorama.
    If they were not a match then they were flipped back again after a short time
    so that the user could see what the cards contained.

--- script.js
+++ script.js
@@ -20,16 +20,20 @@ function shuffleArray(array) {
 
 //Here we creat the array for the words to be used in the memorama
 let words = [];
+let wordMap = [];
 function modifyWords(n){
     words = [];
     let j=0;
     shuffleArray(bodyParts);
+    wordMap = bodyParts.slice(0,n);
     for(let i=0;i<n;i++){
         words[j++] = bodyParts[i].french;
         words[j++] = bodyParts[i].english; 
     }
     shuffleArray(words);
 }
+
+//Creation and deletion of the grid
 
 const grid = document.querySelector('.grid-container');
 
@@ -78,10 +82,50 @@ function createGrid(nRows){
 
 }
 
-function flipCard(element) {
-    element.classList.toggle('is-flipped');
+//word pair check
+function check(french,english){
+    let opOne = wordMap.find(part=>part.french===french && part.english===english);
+    let opTwo = wordMap.find(part=>part.french===english && part.english===french);
+
+    return (opOne||opTwo);
 }
 
+//FLip mechanics
+
+let flipCount = 0;
+let flag = false;
+function flipCard(element) {
+    if(flag || element.classList.contains('disabled'))
+        return;
+
+    element.classList.toggle('is-flipped');
+    if(++flipCount===2){
+        flag = true;
+
+        let pairW = document.querySelectorAll('.is-flipped:not(.disabled)');
+        let wOne = pairW[0].textContent;
+        let wTwo = pairW[1].textContent;
+        let res = check(wOne,wTwo);
+
+        if(res){
+            console.log("Theyre a match");
+            pairW[0].classList.add('disabled');
+            pairW[1].classList.add('disabled');
+            flag = false;
+            }else{
+            setTimeout(() => {
+                pairW[0].classList.toggle('is-flipped');
+                pairW[1].classList.toggle('is-flipped');
+                console.log("Nope");
+                flag = false;
+              }, 2000);
+              
+            }
+        flipCount=0;
+    }
+}
+
+//Play mechanics
 const playButton = document.querySelector('.play-button');
 playButton.addEventListener('click',function(){
     const option = document.getElementById('size');
